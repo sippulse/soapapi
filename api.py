26dac22884fd5130598c141871781131b5a842e2
Eliakim Feliciano Morais <eliakim@sippulse.com>
2019-07-03 11:43:22 -0300

    Adição de novos scripts Python para inserção de usuário via WS

---
+++ api.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+import requests
+
+from zeep import Client
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from zeep.transports import Transport
+
+session = Session()
+session.auth = HTTPBasicAuth('integracao', '#integracao#')
+addressWsdl = "http://189.90.58.142:8080/SipPulse/AddressWS?wsdl"
+transport = Transport(session=session)
+
+
+
+
+def insertAddress(accountcode, domain, ipAddress, mask, method, port, protocol, username, techprefix, login, password): 
+    client = Client(addressWsdl, transport=transport)
+
+   
+    address = {
+        'address' : [{
+            'account': accountcode,
+            'domain': domain,
+            'ipAddress': ipAddress, 
+            'mask': mask,
+            'method': method,
+            'username': username, 
+            'techPrefix': techprefix,
+            'protocol': protocol,
+            'port': port,
+            'group': '1000',
+        }],
+        'principal' : [{
+            'login': login,
+            'password': password,
+        }]
+
+    }
+    print(dir(client.service))
+    #response = client.service.sendData(**address)
+    response = client.service.insertAddress(**address)
+
+insertAddress('eliakim@189.90.58.141', '189.90.58.141', '189.90.58.138', '32' 'techprefix','5060', 'udp','eliakim','9197','integracao','#integracao#')
